5f13616def693f28252b33c7187f627480bf6faf
KaweVK <victorkawe07@gmail.com>
2026-04-11 00:23:08 -0300

    First: Primeiro commit do backend

---
+++ backend/src/main/java/kawe/vk/me_audota/MeAudotaApplication.java
@@ -0,0 +1,13 @@
+package kawe.vk.me_audota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeAudotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeAudotaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kawe/vk/me_audota/MeAudotaApplicationTests.java
@@ -0,0 +1,13 @@
+package kawe.vk.me_audota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeAudotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
