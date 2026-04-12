43f5971736927c98a1f474142c258ef82ac5edad
KaweVK <victorkawe07@gmail.com>
2026-04-12 18:57:48 -0300

    Feat: Pet DTO

---
+++ backend/src/main/java/kawe/vk/me_audota/dto/PetDTO.java
@@ -0,0 +1,33 @@
+package kawe.vk.me_audota.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import kawe.vk.me_audota.model.enums.Especie;
+import kawe.vk.me_audota.model.enums.StatusPet;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public record PetDTO(
+        BigInteger id,
+        @NotNull
+        @Size(min = 1, max = 100)
+        @NotBlank
+        String nome,
+        List<String> imagens,
+        @Size(max = 200)
+        String descricao,
+        @NotNull
+        BigInteger idadeMes,
+        @NotNull
+        BigInteger idadeAno,
+        @NotNull
+        Especie especie,
+        @NotNull
+        String cor,
+        String sexo,
+        @NotNull
+        StatusPet status
+) {
+}
