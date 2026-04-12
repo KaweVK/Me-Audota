03b130268b3c79b827f4adcb55b28528bd875c96
KaweVK <victorkawe07@gmail.com>
2026-04-12 18:45:15 -0300

    Feat: Pet Repository

---
+++ backend/src/main/java/kawe/vk/me_audota/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package kawe.vk.me_audota.repository;
+
+import kawe.vk.me_audota.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Integer> {
+}
