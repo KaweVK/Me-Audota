0a9e0ee9842355c3e7c1767953ff1cd36866f0fb
KaweVK <victorkawe07@gmail.com>
2026-04-12 20:52:23 -0300

    Fix: Corrigindo mapStruct

--- backend/src/main/java/kawe/vk/me_audota/mapper/PetMapper.java
+++ backend/src/main/java/kawe/vk/me_audota/mapper/PetMapper.java
@@ -3,13 +3,9 @@ package kawe.vk.me_audota.mapper;
 import kawe.vk.me_audota.dto.PetDTO;
 import kawe.vk.me_audota.model.Pet;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface PetMapper {
-
-    PetMapper INSTANCE = Mappers.getMapper( PetMapper.class );
-
      Pet toEntity(PetDTO petDTO);
 
      PetDTO toDTO(Pet pet);
