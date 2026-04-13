2408882d4955845af185cf04ee8010292a664f3a
KaweVK <victorkawe07@gmail.com>
2026-04-12 21:29:19 -0300

    Fix: Adicionando JPAAuditing

--- backend/src/main/java/kawe/vk/me_audota/MeAudotaApplication.java
+++ backend/src/main/java/kawe/vk/me_audota/MeAudotaApplication.java
@@ -2,8 +2,10 @@ package kawe.vk.me_audota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MeAudotaApplication {
 
 	public static void main(String[] args) {
