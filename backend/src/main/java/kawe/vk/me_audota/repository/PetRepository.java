809d86bbb894b4d51887a5fe0556bd5f84d545cd
KaweVK <victorkawe07@gmail.com>
2026-04-14 22:41:56 -0300

    Feat: Melhorias nos repositorys para melhorar a recuperação dos pets e usuários

--- backend/src/main/java/kawe/vk/me_audota/repository/PetRepository.java
+++ backend/src/main/java/kawe/vk/me_audota/repository/PetRepository.java
@@ -1,7 +1,21 @@
 package kawe.vk.me_audota.repository;
 
 import kawe.vk.me_audota.model.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    @Query("SELECT p FROM Pet p LEFT JOIN FETCH p.imagens WHERE p.id = :id")
+    Optional<Pet> findById(@Param("id") Long id);
+
+    @EntityGraph(attributePaths = {"imagens"})
+    Page<Pet> findAll(Pageable pageable);
 }
--- backend/src/main/java/kawe/vk/me_audota/repository/UsuarioRepository.java
+++ backend/src/main/java/kawe/vk/me_audota/repository/UsuarioRepository.java
@@ -1,11 +1,23 @@
 package kawe.vk.me_audota.repository;
 
+import kawe.vk.me_audota.model.Pet;
 import kawe.vk.me_audota.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+     @Query("SELECT u FROM Usuario u LEFT JOIN FETCH u.petsAnunciados WHERE u.id = :id")
+     Optional<Usuario> findById(@Param("id") Long id);
+
+     @EntityGraph(attributePaths = {"petsAnunciados"})
+     Page<Usuario> findAll(Pageable pageable);
 
      Optional<Usuario> findByEmail(String email);
 }
